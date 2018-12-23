ea6fac0de2778faa70ca6ba3d906eece8aeed105
victorcarpiomrf <victor.carpio@marfeel.com>
2018-12-23 10:34:21 +0100

    Added credentials for post

--- src/components/ArticleCreate.vue
+++ src/components/ArticleCreate.vue
@@ -1,53 +1,61 @@
 <template>
-	<div class="creator">
-		<h3>Insert Post</h3>
-		 <form action="" method="post">
-			<fieldset>
-				<input name="author" placeholder="Author" type="text" tabindex="1" required autofocus>
-			</fieldset>
-			<fieldset>
-				<input name="subtitle" placeholder="Subtitle" type="text" tabindex="2" required>
-			</fieldset>
-			<fieldset>
-				<input name="media" placeholder="Image" type="url" tabindex="3" required>
-			</fieldset>
-			<fieldset>
-				<textarea name="excerpt" placeholder="Content" tabindex="4" required></textarea>
-			</fieldset>
-			<input type="hidden" name="date" :value="new Date()">
-		</form>
-		<div class="insert-post">
-			Insert Post
-		</div>
-	</div>
+    <div class="creator">
+        <h3>Insert Post</h3>
+         <form action="" method="post">
+            <fieldset>
+                <input name="author" placeholder="Author" type="text" tabindex="1" required autofocus>
+            </fieldset>
+            <fieldset>
+                <input name="subtitle" placeholder="Subtitle" type="text" tabindex="2" required>
+            </fieldset>
+            <fieldset>
+                <input name="media" placeholder="Image" type="url" tabindex="3" required>
+            </fieldset>
+            <fieldset>
+                <textarea name="excerpt" placeholder="Content" tabindex="4" required></textarea>
+            </fieldset>
+            <input type="hidden" name="date" :value="new Date()">
+        </form>
+        <fieldset>
+                <input name="MongoDB Key" class="mongo-key" type="password" placeholder="MongoDB Key" tabindex="5" required>
+        </fieldset>
+        <div class="insert-post">
+            Insert Post
+        </div>
+    </div>
 </template>
 
 <script>
 function buildPostJson() {
-	this.post = {};
-	this.inputs.forEach(field => {
-		this.post[field.name] = field.value;
-	});
+    this.post = {};
+    this.inputs.forEach(field => {
+        this.post[field.name] = field.value;
+    });
 
-	return this.post;
+    return this.post;
 }
 
 function insertPost() {
-	fetch('http://localhost:3000/api/add', {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(buildPostJson.call(this))
-	})
-	.then(response => response.json())
-	.then(response => {
-		this.$el.querySelector('form').reset();
-		console.log(response);
-		alert('All nice!');
-	})
-	.catch(error => console.log(error));
+    const newPost = buildPostJson.call(this);
+    const mongoKey = this.mongoKey.value;
+
+    fetch('http://localhost:3000/api/add', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ newPost, mongoKey })
+    })
+    .then(response => response.json({ mongoKey, newPost }))
+    .then(response => {
+        if (!!response.ok) {
+            this.$el.querySelector('form').reset();
+            alert('All nice!');
+        }
+        console.log(response)
+    })
+    .catch(error => {console.log(error));
 }
 
 export default {
@@ -55,33 +63,34 @@ export default {
   components: {},
   props: {},
   data() {
-	  return {
-		  insertButton: null,
-		  post: null,
-		  inputs: null
-	  }
+      return {
+          insertButton: null,
+          post: null,
+          inputs: null
+      }
   },
   methods: {
-	  updateVisualizer: function() { this.$emit('updateVisualizer', buildPostJson.call(this)) }
+      updateVisualizer: function() { this.$emit('updateVisualizer', buildPostJson.call(this)) }
   },
   mounted: function() {
-	  this.inputs = Array.from(this.$el.querySelectorAll('input, textarea'));
-	  this.insertButton = this.$el.querySelector('.insert-post');
+      this.inputs = Array.from(this.$el.querySelectorAll('form input, form textarea'));
+      this.mongoKey = this.$el.querySelector('.mongo-key');
+      this.insertButton = this.$el.querySelector('.insert-post');
 
-	  this.insertButton.addEventListener('click', insertPost.bind(this));
-	  this.inputs.forEach(field => field.addEventListener('keyup', this.updateVisualizer.bind(this)));
+      this.insertButton.addEventListener('click', insertPost.bind(this));
+      this.inputs.forEach(field => field.addEventListener('keyup', this.updateVisualizer.bind(this)));
   }
 };
 </script>
 
 <style scoped>
 .creator {
-	flex-grow: 1;
-	margin: 0 2%;
+    flex-grow: 1;
+    margin: 0 2%;
 }
 
 form {
-	width: 98%;
+    width: 98%;
 }
 
 h3 {
@@ -113,14 +122,14 @@ textarea {
 }
 
 .insert-post {
-	border-radius: 5px;
-	padding: 5px 11px;
-	border: 1px solid black;
-	margin-top: 20px;
-	text-align: center;
-	width: 92%;
-	color: white;
-	background-color: #E84C3D;
+    border-radius: 5px;
+    padding: 5px 11px;
+    border: 1px solid black;
+    margin-top: 20px;
+    text-align: center;
+    width: 92%;
+    color: white;
+    background-color: #E84C3D;
 }
 
 </style>
